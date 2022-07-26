e84fbf5935a8dcb68f310e135692218769ca78ec
JitaBo1418 <50240@cru.ac.th>
2022-07-26 14:49:33 +0700

    function

---
+++ work-function.py
@@ -0,0 +1,10 @@
+from math import pi
+
+
+def square(width, height):
+    return(width * height)
+def triangle(base,height):
+    return(1/2*base * height) 
+def circle(radius):
+    return(pi*radius*radius)
+#นางสาวจิรัชยา บุญอากาศ ชั้น ม.6/14 เลขที่ 35
